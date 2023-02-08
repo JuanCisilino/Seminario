116c9ee7350ade473a2c24f7c7aaf7d75a9c0c38
Juan Cisilino <54241344+JuanCisilino@users.noreply.github.com>
2023-02-08 11:39:10 -0300

    clase 4 Creacion de vectores e icono Permisos de ubicacion Patron de diseño Decorator Integracion a Firebase Storage

--- app/src/main/java/com/avalith/seminariokotlin/repositories/FirebaseRepo.kt
+++ app/src/main/java/com/avalith/seminariokotlin/repositories/FirebaseRepo.kt
@@ -1,9 +1,12 @@
 package com.avalith.seminariokotlin.repositories
 
+import android.net.Uri
 import com.avalith.seminariokotlin.model.UserData
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 
 class FirebaseRepo {
@@ -11,6 +14,8 @@ class FirebaseRepo {
     private val userDb = Firebase.firestore
 
     private val database = FirebaseDatabase.getInstance()
+
+    private val storageReference = FirebaseStorage.getInstance().getReference("Post Images")
 
     fun saveUser(user: UserData){
         userDb.collection("users").document(user.email?:"").set(
@@ -25,4 +30,9 @@ class FirebaseRepo {
     fun getUser(email: String) = userDb.collection("users").document(email).get()
 
     fun getPosts() = database.getReference("posts")
+
+    fun getStorageReference() = storageReference
+
+    fun saveImageToDb(reference: StorageReference, uri: Uri) = reference.putFile(uri)
+
 }
